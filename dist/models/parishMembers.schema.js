f4a5a3890ddd93b467399175259ff32a850c8e9e
GeorgeJose <george@kumohq.co>
2024-07-19 16:36:19 +0530

    fix issues

---
+++ dist/models/parishMembers.schema.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// models/ParishMember.ts
+const mongoose_1 = require("mongoose");
+// Create a Schema corresponding to the document interface.
+const ParishMemberSchema = new mongoose_1.Schema({
+    name: { type: String, required: true },
+    houseName: { type: String, required: true },
+    image: { type: String },
+    category: {
+        type: String,
+        required: true,
+        enum: ['spiritual-leaders', 'parish-council', 'eminent-personalities', 'parish-members']
+    },
+});
+// Create a Model.
+const ParishMember = (0, mongoose_1.model)('ParishMember', ParishMemberSchema);
+exports.default = ParishMember;
--- src/services/cloudinary.ts
+++ src/services/cloudinary.ts
@@ -3,11 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// cloudinary.config({
+//   cloud_name: "dyawq6e7r",
+//   api_key: "844611149398845",
+//   api_secret: "xn9B4Tmz_x_hYWe6JbwQGZgmJuk",
+//   secure: true,
+// });
+
 cloudinary.config({
-  cloud_name: "dyawq6e7r",
-  api_key: "844611149398845",
-  api_secret: "xn9B4Tmz_x_hYWe6JbwQGZgmJuk",
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
+
+
 
 export default cloudinary;
